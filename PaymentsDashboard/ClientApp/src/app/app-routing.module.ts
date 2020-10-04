dd85de0d22295e34d9060c98956b4f259384ebdb
Michael Holley <michael.philipp.holley@gmail.com>
2020-10-04 15:29:40 +0200

    routing added

---
+++ PaymentsDashboard/ClientApp/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PaymentsComponent } from './payments/payments.component';
+import { TagsComponent } from './tags/tags.component';
+
+const routes: Routes = [
+  { path: '', component: PaymentsComponent },
+  { path: 'Payments', component: PaymentsComponent },
+  { path: 'Tags', component: TagsComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
