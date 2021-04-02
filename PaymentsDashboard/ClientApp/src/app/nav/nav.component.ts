63424a18013b71612595093242bf45c3a23ba8d2
Michael Holley <michael.philipp.holley@gmail.com>
2021-04-02 16:04:34 +0200

    icons added to navigation

--- PaymentsDashboard/ClientApp/src/app/nav/nav.component.ts
+++ PaymentsDashboard/ClientApp/src/app/nav/nav.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { faMoneyBillWave, faTag } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html'
 })
 export class NavComponent implements OnInit {
+  faTag = faTag;
+  faMoneyBillWave = faMoneyBillWave;
 
   expandedOnMobile: boolean = false;
 
