ae066e3ff6e69dd40d03936043396d5b151c76f6
Michael Holley <michael.philipp.holley@gmail.com>
2021-04-30 18:03:32 +0200

    scatter and bar chart

--- PaymentsDashboard/ClientApp/src/app/statistics/statistics.component.ts
+++ PaymentsDashboard/ClientApp/src/app/statistics/statistics.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { PaymentService } from '../../assets/shared/services/payment.service';
 import { TagService } from '../../assets/shared/services/tag.service';
-import { ChartComponent, ApexAxisChartSeries, ApexChart, ApexXAxis, ApexYAxis, ApexDataLabels, ApexGrid, ApexTitleSubtitle } from "ng-apexcharts";
+import { ChartComponent, ApexAxisChartSeries, ApexChart, ApexXAxis, ApexYAxis, ApexTitleSubtitle, ApexDataLabels, ApexPlotOptions } from "ng-apexcharts";
+import { Payment, Tag } from '../../assets/shared/models/models';
 
 export type ChartOptions = {
   series: ApexAxisChartSeries;
@@ -10,7 +11,7 @@ export type ChartOptions = {
   yaxis: ApexYAxis;
   title: ApexTitleSubtitle;
   dataLabels: ApexDataLabels;
-  grid: ApexGrid;
+  plotOptions: ApexPlotOptions;
 };
 
 @Component({
@@ -18,48 +19,116 @@ export type ChartOptions = {
   templateUrl: './statistics.component.html'
 })
 export class StatisticsComponent implements OnInit {
-  public chartOptions: Partial<ChartOptions>;
+  public scatterChartOptions: Partial<ChartOptions>;
+  public dailySumChartOptions: Partial<ChartOptions>;
 
   constructor(
     private paymentsService: PaymentService,
     private tagService: TagService) { }
 
   ngOnInit() {
-    this.chartOptions = {
-      title: { text: 'Payments by Tag' },
-      chart: {
-        type: 'scatter',
-        zoom: {
-          type: 'xy'
-        }
-      },
-      series: [],
-      xaxis: {
-        type: 'datetime'
-      },
-      yaxis: {
-        labels: {
-          formatter: function (val) {
-            return val.toFixed(2);
+    // Scatter-Chart
+    {
+      this.scatterChartOptions = {
+        title: { text: 'Payments by Tag' },
+        chart: {
+          type: 'scatter',
+          zoom: {
+            type: 'xy'
+          }
+        },
+        series: [],
+        xaxis: {
+          type: 'datetime'
+        },
+        yaxis: {
+          labels: {
+            formatter: this.valueFormatter
           }
         }
-      }
-    };
+      };
 
-
-    this.tagService.getAllTags().subscribe(result => {
-      result.forEach(tag => {
-        let paymentValues = [];
-        tag.payments.forEach(p => paymentValues.push([new Date(p.date).getTime(), p.amount]));
-        console.log(paymentValues)
-        this.chartOptions.series.push(
-          {
-            name: tag.title,
-            color: tag.hexColorCode,
-            data: paymentValues
-          }
-        );
+      this.tagService.getAllTags().subscribe(result => {
+        result.forEach(tag => {
+          let values = [];
+          tag.payments.forEach(p => values.push([new Date(p.date).getTime(), p.amount]));
+          this.scatterChartOptions.series.push(
+            {
+              name: tag.title,
+              color: tag.hexColorCode,
+              data: values
+            }
+          );
+        });
       });
+    }
+
+    // Stacked Bar-Chart
+    {
+      this.dailySumChartOptions = {
+        title: { text: 'Sum of Payments by Date' },
+        chart: {
+          type: 'bar'
+        },
+        series: [],
+        yaxis: {
+          labels: {
+            formatter: this.valueFormatter
+          }
+        },
+        dataLabels: {
+          enabled: false,
+          formatter: this.valueFormatter
+        }
+      };
+
+      this.paymentsService.getAllPayments().subscribe(payments => {
+        let paymentsByDate: {
+          date: Date,
+          sum: number
+        }[] = [];
+
+        let startDate = new Date(payments[0].date);
+        let lastDate = new Date(payments[payments.length - 1].date);
+
+        while (startDate <= lastDate) {
+          let tempSum = this.getSum(payments.filter(p => new Date(p.date).getTime() === startDate.getTime()));
+          paymentsByDate.push({ date: new Date(startDate), sum: tempSum });
+          startDate.setDate(startDate.getDate() + 1);
+        }
+
+        let dates = [];
+        let values = [];
+        paymentsByDate.forEach(pbd => {
+          dates.push(pbd.date);
+          values.push(pbd.sum);
+        });
+
+        this.dailySumChartOptions.series.push({
+          name: 'Payments',
+          color: '#FF6B35',
+          data: values
+        });
+
+        this.dailySumChartOptions.xaxis = {
+          type: 'datetime',
+          categories: dates
+        };
+      });
+    }
+  }
+
+
+  getSum(payments: Payment[]): number {
+    let sum = 0;
+    payments.forEach(p => {
+      sum += p.amount;
     });
+
+    return sum;
+  }
+
+  valueFormatter(val) {
+    return val.toFixed(2);
   }
 }
