1b48721e4bd176094874727ce37b2afa814d26c1
Michael Holley <michael.philipp.holley@gmail.com>
2021-06-05 14:14:43 +0200

    Basic Controller implementation

--- PaymentsDashboard/Controllers/StatisticsController.cs
+++ PaymentsDashboard/Controllers/StatisticsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PaymentsDashboard.Data;
+using PaymentsDashboard.Data.Modells;
 using PaymentsDashboard.Data.Modells.Statistics;
 using PaymentsDashboard.Services;
 using System;
@@ -50,6 +51,40 @@ namespace PaymentsDashboard.Controllers
 
 				values.Add(temp);
 				startDate = startDate.AddMonths(1);
+			}
+
+			return Ok(values);
+		}
+
+		[HttpGet("[action]")]
+		public ActionResult<IEnumerable<TagSum>> GetMonthlyAverageByTag()
+		{
+			var payments = paymentService.GetAllPayments().ToList();
+			var tags = tagService.GetPrimaryTags().RemoveCycle();
+			var values = new List<TagSum>();
+
+			var startDate = new DateTime(DateTime.Parse(payments.First().Date).Year, DateTime.Parse(payments.First().Date).Month, 1);
+			var finalDate = new DateTime(DateTime.Parse(payments.Last().Date).Year, DateTime.Parse(payments.Last().Date).Month + 1, 1);
+
+			foreach (var tag in tags)
+			{
+				values.Add(new TagSum() { Tag = tag, Sum = 0 });
+			}
+
+			var visitedMonths = new List<DateTime>();
+			foreach (var p in payments)
+			{
+				var monthOfPayment = new DateTime(DateTime.Parse(p.Date).Year, DateTime.Parse(p.Date).Month, 1);
+				if (!visitedMonths.Any(vm => vm.Equals(monthOfPayment))) {
+					visitedMonths.Add(monthOfPayment);
+				}
+
+				values.Single(v => v.Tag.TagId.Equals(p.Tags.Single(pT => pT.Type.Equals(TagType.Primary)).TagId)).Sum += p.Amount;
+			}
+
+			foreach(var val in values)
+			{
+				val.Sum /= visitedMonths.Count;
 			}
 
 			return Ok(values);
