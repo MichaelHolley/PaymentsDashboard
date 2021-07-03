9d01e022c521b9ac2271d6cd85a4c783484d237d
Michael Holley <michael.philipp.holley@gmail.com>
2021-07-03 15:50:57 +0200

    Implementation and Migration

---
+++ PaymentsDashboard/Data/Modells/ReoccuringPayment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PaymentsDashboard.Data.Modells
+{
+	public class ReoccuringPayment : CreatedBase
+	{
+		public Guid Id { get; set; }
+		public string Title { get; set; }
+		public decimal Amount { get; set; }
+		public string StartDate { get; set; }
+		public string EndDate { get; set; }
+		public ReoccuringType ReoccuringType { get; set; }
+		public ICollection<Tag> Tags { get; set; }
+	}
+}
