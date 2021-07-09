4480f79969be41521ae34dbfd5dfe75eca2f4f3c
Michael Holley <michael.philipp.holley@gmail.com>
2021-07-09 17:10:10 +0200

    Migration added

---
+++ PaymentsDashboard/Migrations/20210709150459_ModificationAwarenessAdded.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PaymentsDashboard.Migrations
+{
+    public partial class ModificationAwarenessAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Modified",
+                table: "Tags",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Modified",
+                table: "ReoccuringPayments",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Modified",
+                table: "Payments",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Modified",
+                table: "Tags");
+
+            migrationBuilder.DropColumn(
+                name: "Modified",
+                table: "ReoccuringPayments");
+
+            migrationBuilder.DropColumn(
+                name: "Modified",
+                table: "Payments");
+        }
+    }
+}
